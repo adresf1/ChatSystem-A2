fc18624f4ded259c021fa28dce5da22fff6284d6
HiroshikunDK <36033304+HiroshikunDK@users.noreply.github.com>
2024-03-26 12:17:11 +0100

    start
    ds

---
+++ src/main/java/com/example/chatsystema2/View/ServerConnection.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class ServerConnection
+{
+    private ObjectInputStream fromClient;
+    private ObjectOutputStream toClient;
+    private ConnectionPool connectionPool;
+
+    public ServerConnection(Socket )
+
+}
